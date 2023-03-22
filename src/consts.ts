3140773a6e57b6d83d79ce53a65bfc99042be88f
Joe Shambrook <jshambrook@and.digital>
2023-03-22 09:19:32 +0000

    First commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Edward Shambrook';
+export const SITE_DESCRIPTION = 'Narrative designer';
