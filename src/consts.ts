2a611c057889d1407b7cf72e0b6d6334f7301da0
Joe Shambrook <jshambrook@and.digital>
2023-03-23 17:06:03 +0000

    overhaul

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Edward Shambrook';
-export const SITE_DESCRIPTION = 'Narrative designer';
+export const SITE_TITLE = "Edward Shambrook";
+export const SITE_DESCRIPTION = "Narrative Designer";
